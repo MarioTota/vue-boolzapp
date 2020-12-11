39e048a27adb71908cc171a4c9f6e0ec6d33051b
Mario <mario.tota.mt@gmail.com>
2020-12-11 21:29:27 +0100

    commit

--- script.js
+++ script.js
@@ -1,11 +1,98 @@
 var app = new Vue({
     el: '#root',
     data: {
-      
+      textIndex: this.messages.slice(-1).pop(),
+
+      contacts: [
+      	{
+      		name: 'Gigi',
+      		avatar: 'https://www.nazionalecantanti.it/wp-content/uploads/2017/05/giocatore-gigi-dalessio.jpg',
+      		visible: true,
+      		messages: [
+      			{
+      				date: '10/01/2020 15:30:55',
+      				text: 'Hai portato a spasso il cane?',
+      				status: 'sent'
+      			},
+      			{
+      				date: '10/01/2020 15:50:00',
+      				text: 'Ricordati di dargli da mangiare',
+      				status: 'sent'
+      			},
+      			{
+      				date: '10/01/2020 16:15:22',
+      				text: 'Tutto fatto!',
+      				status: 'received'
+      			}
+      		],
+      	},
+      	{
+      		name: 'Tiziano',
+      		avatar: 'https://www.occhionotizie.it/wp-content/uploads/2020/05/Schermata-2019-02-21-alle-12.38.17.png',
+      		visible: true,
+      		messages: [
+      			{
+      				date: '20/03/2020 16:30:00',
+      				text: 'Ciao come stai?',
+      				status: 'sent'
+      			},
+      			{
+      				date: '20/03/2020 16:30:55',
+      				text: 'Bene grazie! Stasera ci vediamo?',
+      				status: 'received'
+      			},
+      			{
+      				date: '20/03/2020 16:35:00',
+      				text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+      				status: 'sent'
+      			}
+      		],
+      	},
+      	{
+      		name: 'Madonna',
+      		avatar: 'https://d2u3vvj2lhotug.cloudfront.net/wp-content/uploads/2020/01/Madonna.jpg',
+      		visible: true,
+      		messages: [
+      			{
+      				date: '28/03/2020 10:10:40',
+      				text: 'La Marianna va in campagna',
+      				status: 'received'
+      			},
+      			{
+      				date: '28/03/2020 10:20:10',
+      				text: 'Sicuro di non aver sbagliato chat?',
+      				status: 'sent'
+      			},
+      			{
+      				date: '28/03/2020 16:15:22',
+      				text: 'Ah scusa!',
+      				status: 'received'
+      			}
+      		],
+      	},
+      	{
+      		name: 'Elettra',
+      		avatar: 'https://img2.liberoquotidiano.it/images/2020/06/04/055156871-875d79ef-0e00-4fd4-8031-c26f46a3b9f6.jpg',
+      		visible: true,
+      		messages: [
+      			{
+      				date: '10/01/2020 15:30:55',
+      				text: 'Lo sai che ha aperto una nuova pizzeria?',
+      				status: 'sent'
+      			},
+      			{
+      				date: '10/01/2020 15:50:00',
+      				text: 'Si, ma preferirei andare al cinema',
+      				status: 'received'
+      			}
+      		],
+      	},
+      ]
+
+
     },
-
     methods: {
-
+      // arr.slice(-1).pop()
 
     }
 
